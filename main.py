449f4b3f9824956a4ffcdcdb967c7b03ea06adcf
Warley Almeida <almeida.warley@outlook.com>
2022-03-03 16:44:50 -0500

    Added argument parser to the main file

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-from inspect import BoundArguments
 import instance as it
 import analytical as an
 import backward as bc
 import random as rd
+import argparse as ag
 
-sim_number = 5
+rd.seed(100)
 
-problem = it.Instance('small.txt')
+def log_policy(policy, stages, states, name):
 
-solver1 = an.Analytical(problem)
-solver2 = bc.Backward(problem)
+    for k in stages:
+        print('Stage {}:'.format(k))
 
-policy1 = solver1.run()
-policy2 = solver2.run()
+        for x in states:
 
-for k in range(0, problem.N):
+            print('\tOptimal {} policy for inventory {} μ*_{}({}) = {} units'.format(name, x, k, x, policy(k, x)))
 
-    for x in range(-problem.peak, problem.peak):
-        print('# Stage {}, state {}'.format(k, x))
-        print('\tOptimal (analytical) policy for an inventory at level {} in stage {} -> order {} units'.format(x, k, policy1(k, x)))
-        print('\tOptimal (backward) policy for an inventory at level {} in stage {} -> order {} units'.format(x, k, policy2(k, x)))
+# Parse command line arguments
+parser = ag.ArgumentParser(description = 'Compute analytical form and backward chaining optimal policies for an inventory problem instance')
+parser.add_argument('path', type = str, help = 'Path to the text file with the instance information')
+parser.add_argument('--evaluations', type = int, help = 'Number of sampled states to evaluate the optimal policy', default = 10)
+arguments = parser.parse_args()
 
-        _ = input('Press enter to move forward...')
+# Read instance information from a text file
+problem = it.Instance(arguments.path)
+
+# Create list of stages from the problem instance
+stages = list(range(0, problem.N))
+
+# Sample states to evaluate the optimal policy
+counter = 0
+states = []
+while counter <= arguments.evaluations:
+    states.append(rd.randint(-1 * problem.peak, problem.peak))
+    counter = counter + 1
+states = sorted(states)
+
+# Create the analytical form solver
+solver = an.Analytical(problem)
+
+# Compute the optimal policy 
+policy = solver.run()
+
+log_policy(policy, stages, states, 'analytical')
+
+# Create the backward chaining solver
+solver = bc.Backward(problem)
+
+# Compute the optimal policy
+policy = solver.run()
+
+log_policy(policy, stages, states, 'backward')
