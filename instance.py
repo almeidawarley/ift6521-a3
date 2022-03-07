97a55097152b4fe7759675bc0b548522f09b9329
Warley Almeida <almeida.warley@outlook.com>
2022-03-07 13:03:12 -0500

    Moved cost functions from instance to solvers

--- instance.py
+++ instance.py
@@ -7,7 +7,7 @@ class Instance:
         # Open file with instance information
         with open(path) as content:
             
-            # Store N, l, u, c, p, h, d
+            # Store N, l, u, c, p, h, e
             self.N = int(content.readline())
             self.l = int(content.readline())
             self.u = int(content.readline())
@@ -16,41 +16,20 @@ class Instance:
             self.h = int(content.readline())
             self.e = int(content.readline())
 
-            # Some sort of cost to dispose items
-            # self.d = int(content.readline())
-
-        # Start switch to modify instance
-        self.switch = switch
-
-        # Store maximum inventory level
+        # Store reasonable inventory level
         self.peak = self.N * self.u
-
-    def flip(self):
-
-        # Flip the switch of the instance
-        self.switch = not self.switch
 
     def __str__(self):
 
         # Return instance information as string
         payload = '# -----------------------------------------------\n'
-        payload += '\tMaximum number of stages (N): {}\n'.format(self.N)
+        payload += '\tNumber of stages (N): {}\n'.format(self.N)
         payload += '\tUniform distribution: U({}, {})\n'.format(self.l, self.u)
         payload += '\tUnit ordering costs (c): {}\n'.format(self.c)
         payload += '\tUnit shortage costs (p): {}\n'.format(self.p)
         payload += '\tUnit storage costs (h): {}\n'.format(self.h)
+        payload += '\tReturnable units (e): {}\n'.format(self.e)
+        payload += '\tInventory peak (peak): {}\n'.format(self.peak)
         payload += '# -----------------------------------------------'
 
         return payload
-
-    def r(self, k, x):
-
-        # Compute cost of inventory level x at stage k
-        return self.p * max(0, -1 * x) + self.h * max(0, x)
-        # return self.p[k] * max(0, -1 * x) + self.h[k] * max(0, x)
-
-    def t(self, k, u):
-        
-        # Compute cost of ordering u units at stage k
-        return self.c * u
-        # return self.c[k] * u
