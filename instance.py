edd368a22ba39373b2c7e9805bfba035b7b562ec
Warley Almeida <almeida.warley@outlook.com>
2022-03-07 13:49:17 -0500

    Added code to work with modified instances

--- instance.py
+++ instance.py
@@ -2,7 +2,7 @@ import statistics as st
 
 class Instance:
 
-    def __init__(self, path, switch = True):
+    def __init__(self, path, version = False):
 
         print('Reading instance information from file: {}'.format(path))
 
@@ -13,10 +13,16 @@ class Instance:
             self.N = int(content.readline())
             self.l = int(content.readline())
             self.u = int(content.readline())
-            self.c = int(content.readline())
-            self.p = int(content.readline())
-            self.h = int(content.readline())
+            self.c = float(content.readline())
+            self.p = float(content.readline())
+            self.h = float(content.readline())
             self.e = int(content.readline())
+
+            self.modified = version
+
+            if self.modified:
+                self.frontier = float(content.readline())
+                self.d = float(content.readline())
 
         # Store reasonable inventory peak
         self.peak = self.N * self.u
@@ -33,7 +39,11 @@ class Instance:
         payload += '\tUnit shortage costs (p): {}\n'.format(self.p)
         payload += '\tUnit storage costs (h): {}\n'.format(self.h)
         payload += '\tReturnable units (e): {}\n'.format(self.e)
-        payload += '\tInventory peak (peak): {}\n'.format(self.peak)
+        payload += '\tInventory peak (peak): {}\n'.format(self.peak)        
+        if self.modified:
+            payload += '\tWorking with modified version\n'
+            payload += '\tFrontier for changing: {}\n'.format(self.frontier)
+            payload += '\tUnit ordering costs (d): {}\n'.format(self.d)
         payload += '# -----------------------------------------------'
 
         return payload
--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import matcher as mt
 # Parse command line arguments
 parser = ag.ArgumentParser(description = 'Compute analytical form and backward chaining optimal policies for an inventory problem instance')
 parser.add_argument('path', type = str, help = 'Path to the text file with the instance information')
+parser.add_argument('--modified', type = bool, help = 'Work with the modified problem with non-linear ordering costs', default = False)
 parser.add_argument('--evaluations', type = int, help = 'Number of sampled states to evaluate the optimal policy', default = 10)
 arguments = parser.parse_args()
 
 # Read instance information from a text file
-problem = it.Instance(arguments.path)
+problem = it.Instance(arguments.path, arguments.modified)
 
 # Create an evaluator with the solvers
 evaluator = mt.Matcher(problem, an.Analytical, bc.Backward, arguments.evaluations)
